40cf04945a870ff5f6f6f4ccac9fab501bf10e30
Prem <prem>
2019-10-12 10:58:13 -0700

    Sample Hello world on gpu

---
+++ README.md
@@ -0,0 +1,15 @@
+# Cuda Sample Helloworld Program
+
+Tested on Nvidia Jetson Nano
+
+## Compiling Program
+
+```bash
+nvcc -o hello hello.cu
+```
+
+## Execute
+
+```bash
+time ./hello
+```
---
+++ helloserial.c
@@ -0,0 +1,13 @@
+//#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    printf("Hello World from CPU!\n");
+    
+    for(int i=0; i < 512000; i++){
+        printf("Hello World from CPU!\n");
+    }
+     
+    return 0;
+}
